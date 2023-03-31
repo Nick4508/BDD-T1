3bbd265ebf71fc7359f0b9d16a972d4d6163ae04
MatiasBV <matias.barrerav@usm.cl>
2023-03-30 23:23:50 -0400

    Creacion de tabla
    Cree la tabla canción

---
+++ BD.py
@@ -0,0 +1,11 @@
+import pyodbc
+conn = pyodbc.connect(Driver='{SQL Server}';
+                      Server=Tarea_BD;
+                      Database=Tarea_1;
+                      Trusted_Connection=yes)
+
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE Cancion(position int NOT NULL, artist_name VARCHAR(56) NOT NULL, song_name VARCHAR(56) NOT NULL, days int NOT NULL, peak_position int(10), peak_position_time VARCHAR(5), peak_streams int, total_streams int, PRYMARY KEY(id))")
+'''
+Abrir archivo, separar en tuplas la info y cuadrarla en la tabla
+'''
