d3c245220071c3451d535774fb4dd95cfe6e2bb4
MatiasBV <matias.barrerav@usm.cl>
2023-04-05 00:03:24 -0400

    avance
    nos faltaba un ; en vez de una ,

--- BD.py
+++ BD.py
@@ -1,8 +1,54 @@
 import pyodbc
-conn = pyodbc.connect(Driver='{SQL Server}';Server=Tarea_BD;Database=Tarea_1;Trusted_Connection=yes)
 
-cursor = conn.cursor()
-cursor.execute("CREATE TABLE Cancion(position int NOT NULL, artist_name VARCHAR(56) NOT NULL, song_name VARCHAR(56) NOT NULL, days int NOT NULL, peak_position int(10), peak_position_time VARCHAR(5), peak_streams int, total_streams int, PRYMARY KEY(id))")
-'''
-Abrir archivo, separar en tuplas la info y cuadrarla en la tabla
-'''
+###############################################################################################################################################
+# archivo:   position   artist_name   song_name   days    top_10    peak_position    peak_position_time    peak_streams    total_streams
+#               int         str         str        int      int         int                 str                 int             int
+###############################################################################################################################################
+# reproduccion:
+#               id PK     song_name     artist_name     fecha_reproduccion      cant_reproducciones        favorito    
+#               int          str           str              date                    int                       bit
+# 
+# lista_favoritos:
+#               id PK      song_name    artist_name         fecha_agregada  
+#               int          str            str                 date
+###############################################################################################################################################
+
+#str_conect = "Driver = {SQL Server}, Server = LAPTOP-QKCFS38M\SQLEXPRESS; Database = Spot-USM; Trusted_Connection = True" #nick
+str_conect = "Driver={SQL Server};Server=LAPTOP-334LDJSK\SQLEXPRESS;Database=Spot-USM;Trusted_Connection=True" #mat
+coneccion = pyodbc.connect(str_conect)
+cursor = coneccion.cursor()
+#cursor.execute("CREATE TABLE reproduccion (id int PRIMARY KEY, song_name VARCHAR(100), artist_name VARCHAR(100), fecha_reproduccion DATE, can_reproducciones bigint, favorito bit)")
+#cursor.execute("CREATE TABLE lista_favoritos (id int PRIMARY KEY, song_name VARCHAR(100), artist_name VARCHAR(100), fecha_agregada DATE)")
+
+
+
+
+
+def main():
+    #cursor.execute("CREATE TABLE repositorio_musica (id int IDENTITY(1,1) PRIMARY KEY, position int, artist_name VARCHAR(100), song_name VARCHAR(100), days int, top_10 int, peak_position bigint, peak_position_time VARCHAR(10), peak_streams bigint, total_streams bigint)")
+    
+    archivo = open("song.csv", "r", encoding= "UTF-8-sig")
+    lineas = archivo.readlines()
+    x = 1
+    while x < len(lineas):
+        lista = []
+        for elemento in lineas:
+            a = elemento.strip().split(";")
+            for i in a:
+                lista.append("'"+i+"'")
+        print(lista,"\n")
+        x+=1
+
+
+    # for x in range(len(lineas)):
+    #     a,b,c,d,e,f,g,h,i = tuple(lineas[x].strip().split(";"))
+    #     # cursor.execute("INSERT INTO repositorio_musica (position,artist_name,song_name,days,top_10,peak_position,peak_time,peak_streams,total_streams) values ("+a+","+b+","+c+","+d+","+e+","+f+","+g+","+h+","+i+")")
+    #     print(lineas[x])
+    #     exit()
+    
+    
+    cursor.commit()
+    cursor.close()
+    coneccion.close()
+if __name__ == "__main__":
+    main()
--- tryouts.py
+++ tryouts.py
@@ -1,13 +1,13 @@
 import pyodbc
 
 #str_conect = "Driver = {SQL Server}, Server = LAPTOP-QKCFS38M\SQLEXPRESS; Database = Spot-USM; Trusted_Connection = True" #nick
-str_conect = "Driver = {SQL Server}, Server = LAPTOP-334LDJSK\SQLEXPRESS; Database = Spot-USM; Trusted_Connection = True" #mat
+str_conect = "Driver={SQL Server};Server=LAPTOP-334LDJSK\SQLEXPRESS;Database=Spot-USM;Trusted_Connection=True" #mat
 coneccion = pyodbc.connect(str_conect)
 cursor = coneccion.cursor()
 
 cursor.execute("CREATE TABLE repositorio_musica (id int IDENTITY(1,1) PRIMARY KEY, position int , artist_name VARCHAR(100), song_name VARCHAR(100), days int, top_10 int, peak_position int, peak_position_time VARCHAR(10), peak_streams int, total_streams int)")
-cursor.execute("CREATE TABLE reproduccion (id int IDENTITY(1,1) PRIMARY KEY, song_name VARCHAR(100), artist_name VARCHAR(100), fecha_reproduccion DATE(YYYY-MM-DD), can_reproducciones bigint, favorito bit)")
-cursor.execute("CREATE TABLE lista_favoritos (id int IDENTITY(1,1) PRIMARY KEY, song_name VARCHAR(100), artist_name VARCHAR(100), fecha_agregada DATE(YYYY-MM-DD)")
+cursor.execute("CREATE TABLE reproduccion (id int IDENTITY(1,1) PRIMARY KEY, song_name VARCHAR(100), artist_name VARCHAR(100), fecha_reproduccion DATE, can_reproducciones bigint, favorito bit)")
+cursor.execute("CREATE TABLE lista_favoritos (id int IDENTITY(1,1) PRIMARY KEY, song_name VARCHAR(100), artist_name VARCHAR(100), fecha_agregada DATE)")
 ###############################################################################################################################################
 # archivo:   position   artist_name   song_name   days    top_10    peak_position    peak_position_time    peak_streams    total_streams
 #               int         str         str        int      int         int                 str                 int             int
@@ -21,5 +21,10 @@ cursor.execute("CREATE TABLE lista_favoritos (id int IDENTITY(1,1) PRIMARY KEY, 
 #               int          str            str                 date
 ###############################################################################################################################################
 archivo = open("song.csv", "r", encoding= "UTF-8")
+i = 0
+for linea in archivo:
+    if i < 4:
+        print(linea)
+        i+=1
 cursor.close()
 coneccion.close()
